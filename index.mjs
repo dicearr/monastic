40c1979d344ede5f5713834016febec6f30b7f72
Diego Ceresuela <dcerearr@gmail.com>
2018-07-12 17:55:45 +0200

    docs: add type signatures

--- index.mjs
+++ index.mjs
@@ -1,9 +1,11 @@
+// compose :: ((b -> c), (a -> b)) -> (a -> c)
 export function compose(f, g) {
   return function(v) {
     return f (g (v));
   };
 }
 
+// constant :: a -> b -> a
 export function constant(v) {
   return function() {
     return v;
@@ -14,34 +16,41 @@ export default function State(run) {
   this.run = run;
 }
 
+// of :: a -> State s a
 State.of = function(value) {
   return new State (function(state) {
-    return {value: value, state: state};
+    return {state: state, value: value};
   });
 };
 
+// get :: State s s
 State.get = new State (function(state) {
-  return {value: state, state: state};
+  return {state: state, value: state};
 });
 
+// modify :: (s -> s) -> State s a
 State.modify = function(f) {
   return new State (function(state) {
-    return {value: null, state: f (state)};
+    return {state: f (state), value: null};
   });
 };
 
-State.prototype.put = function(state) {
+// put :: s -> State s a
+State.put = function(state) {
   return State.modify (constant (state));
 };
 
+// eval :: State s a ~> s -> a
 State.prototype.eval = function(state) {
   return this.run (state).value;
 };
 
+// exec :: State s a ~> s -> s
 State.prototype.exec = function(state) {
   return this.run (state).state;
 };
 
+// chain :: State s a ~> (a -> State s b) -> State s b
 State.prototype.chain = function(f) {
   return new State (s => {
     const r = this.run (s);
@@ -49,6 +58,7 @@ State.prototype.chain = function(f) {
   });
 };
 
+// map :: State s a ~> (a -> b) -> State s b
 State.prototype.map = function(f) {
   return this.chain (compose (f, State.of));
 };
@@ -65,34 +75,41 @@ State.StateT = function(M) {
     this.run = run;
   }
 
+  // of :: a -> StateT s a
   StateT.of = function(value) {
      return new StateT (function(state) {
-       return M.of ({value: value, state: state});
+       return M.of ({state: state, value: value});
      });
    };
 
+  // get :: StateT s s
   StateT.get = new StateT (function(state) {
-    return M.of ({value: state, state});
+    return M.of ({state: state, value: state});
   });
 
+  // modify :: (s -> s) -> StateT s a
   StateT.modify = function(f) {
     return new StateT (function(state) {
-      return M.of ({value: null, state: f (state)});
+      return M.of ({state: f (state), value: null});
     });
   };
 
+  // put :: s -> StateT s a
   StateT.put = function(state) {
     return StateT.modify (constant (state));
   };
 
+  // eval :: StateT s a ~> s -> a
   StateT.prototype.eval = function(state) {
     return this.run (state).value;
   };
 
+  // exec :: StateT s a ~> s -> s
   StateT.prototype.exec = function(state) {
     return this.run (state).state;
   };
 
+  // chain :: StateT s a ~> (a -> StateT s b) -> StateT s b
   StateT.prototype.chain = function(f) {
     return new StateT (s => {
       const result = this.run (s);
@@ -100,6 +117,7 @@ State.StateT = function(M) {
     });
   };
 
+  // map :: StateT s a ~> (a -> b) -> StateT s b
   StateT.prototype.map = function(f) {
     return this.chain (compose (f, StateT.of));
   };
