a31b417b09345fb5373bdccb5ee0a50af0e9f444
Diego Ceresuela <dcerearr@gmail.com>
2018-07-16 08:54:57 +0200

    test: add intput parameter check to modify

--- test/state.test.js
+++ test/state.test.js
@@ -7,19 +7,19 @@ import {
   execState
 } from '..';
 
-suite ('State', function() {
-  test ('#modify', function() {
+suite ('Functions', function() {
+  test ('modify', function() {
     var state = Math.random ();
-    var res = modify (_ => state).run ();
-    assert.deepStrictEqual (res.state, state);
+    var res = modify (x => x + 1).run (state);
+    assert.deepStrictEqual (res.state, state + 1);
     assert.equal (res.value, undefined);
   });
-  test ('#put', function() {
+  test ('put', function() {
     var state = Math.random ();
     var res = execState () (put (state));
     assert.deepStrictEqual (res, state);
   });
-  test ('#get', function() {
+  test ('get', function() {
     var state = Math.random ();
     var res = put (state)['fantasy-land/chain'] (
       function() { return get; }
