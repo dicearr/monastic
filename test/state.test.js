67a343b3c2bbca2dbe132df61c32a59d4085bcf4
Diego Ceresuela <dcerearr@gmail.com>
2021-04-28 09:04:07 +0200

    Fix State.fantasy-land/ap
    Initial state is now passed down to both State instances.
    
    Co-Authored-By: Aldwin Vlasblom <aldwin@avaq.it>

--- test/state.test.js
+++ test/state.test.js
@@ -19,6 +19,7 @@ import {
   State,
   run,
   compose,
+  constant,
 } from '../index.js';
 
 const property = _property (test);
@@ -88,6 +89,11 @@ test ('.get sets the internal value to its state', () => {
 
 test ('.run returns internal state an value', () => {
   eq (run (42) (get), {state: 42, value: 42});
+});
+
+test ('.fantasy-land/ap propagates the initial state to both State instances', () => {
+  eq (evalState (42) (Z.ap (Z.of (State, x => x), get)), 42);
+  eq (evalState (42) (Z.ap (Z.map (constant, get), put (null))), 42);
 });
 
 test ('.fantasy-land/chainRec should be stack-safe', () => {
